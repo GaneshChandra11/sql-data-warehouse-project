896d5c8991961fb2b151577a389a301d6cae73c5
GaneshChandra11 <ganeshchandra.cs.it@gmail.com>
2025-08-28 22:40:40 +0530

    data_set added

--- scripts/datawarehouseQuery.sql
+++ scripts/datawarehouseQuery.sql
@@ -158,5 +158,177 @@ create table bronze.erp_px_cat_g1v2(
 );
 
 select * from bronze.erp_px_cat_g1v2;
+GO
+--------------------------------------------Data Ingestion into Bronze Layer---------------------------------------------------------------
+--exec bronze.load_bronze;
 
+CREATE or ALTER PROCEDURE bronze.load_bronze as 
+BEGIN
+    DECLARE @start_time DATETIME, @end_time DATETIME;
+-----data insertion into bronze.crm_cust_info tables
+    BEGIN TRY
+        PRINT '===============================================';
+        PRINT 'Loading the bronze layer';
+        PRINT '===============================================';
+
+        PRINT '-----------------------------------------------';
+        PRINT 'Loading the CRM Tables';
+        PRINT '-----------------------------------------------';
+
+        set @start_time = GETDATE();
+        -----data insertion into bronze.crm_cust_info tables
+        PRINT '-----------------------------------------------';
+        PRINT '>>Truncating Table: bronze.crm_cust_info';
+        print '-----------------------------------------------';
+        TRUNCATE TABLE bronze.crm_cust_info;
+
+        PRINT '-----------------------------------------------';
+        PRINT '>>Inserting Data into: bronze.crm_cust_info';
+        PRINT '-----------------------------------------------';
+        BULK INSERT bronze.crm_cust_info
+        from 'D:\Projects\MS SQL & MSBI BY GURU PRASAD\Data Warehouse\git_repo\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+        WITH
+        (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '\n',
+            TABLOCK
+        );
+
+        --select * from bronze.crm_cust_info;
+        select count(*) from bronze.crm_cust_info;
+        set @end_time = GETDATE();
+        PRINT 'Time taken to load CRM Customer Info Table: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+
+        -----data insertion into bronze.crm_prd_info tables
+
+        PRINT '-----------------------------------------------';
+        PRINT '>>Truncating Table: bronze.crm_prd_info';
+        PRINT '-----------------------------------------------';
+        TRUNCATE TABLE bronze.crm_prd_info;
+
+        PRINT '-----------------------------------------------';
+        PRINT '>>Inserting Data into: bronze.crm_prd_info';
+        PRINT '-----------------------------------------------';
+        BULK INSERT bronze.crm_prd_info
+        from 'D:\Projects\MS SQL & MSBI BY GURU PRASAD\Data Warehouse\git_repo\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+        WITH
+        (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '\n',
+            TABLOCK
+        );
+
+        --select * from bronze.crm_prd_info;
+        select count(*) from bronze.crm_prd_info;
+
+        -----data insertion into bronze.crm_sales_details tables
+        set @start_time = GETDATE();
+        PRINT '-----------------------------------------------';
+        PRINT '>>Truncating Table: bronze.crm_sales_details';
+        PRINT '-----------------------------------------------';
+        TRUNCATE TABLE bronze.crm_sales_details;
+
+        PRINT '-----------------------------------------------';
+        PRINT '>>Inserting Data into: bronze.crm_sales_details';
+        PRINT '-----------------------------------------------';
+        BULK INSERT bronze.crm_sales_details
+        from 'D:\Projects\MS SQL & MSBI BY GURU PRASAD\Data Warehouse\git_repo\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+        WITH
+        (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '\n',
+            TABLOCK
+        );
+
+        --select * from bronze.crm_sales_details;
+        select count(*) from bronze.crm_sales_details;  
+        set @end_time = GETDATE();
+        PRINT 'Time taken to load CRM Tables: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+
+
+        PRINT '-----------------------------------------------';
+        PRINT 'Loading the ERP Tables';
+        PRINT '-----------------------------------------------';
+
+
+        -----data insertion into bronze.erp_loc_a101 tables
+        
+        PRINT '-----------------------------------------------';
+        PRINT '>>Truncating Table: bronze.erp_loc_a101';
+        PRINT '-----------------------------------------------';
+        TRUNCATE TABLE bronze.erp_loc_a101;
+
+        PRINT '-----------------------------------------------';
+        PRINT '>>Inserting Data into: bronze.erp_loc_a101';
+        PRINT '-----------------------------------------------';
+        BULK INSERT bronze.erp_loc_a101
+        from 'D:\Projects\MS SQL & MSBI BY GURU PRASAD\Data Warehouse\git_repo\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+        WITH
+        (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '\n',
+            TABLOCK
+        );
+
+        --select * from bronze.erp_loc_a101;
+        select count(*) from bronze.erp_loc_a101;
+
+        -----data insertion into bronze.erp_cust_az12 tables
+
+        PRINT '-----------------------------------------------';
+        PRINT '>>Truncating Table: bronze.erp_cust_az12';   
+        PRINT '-----------------------------------------------';
+        TRUNCATE TABLE bronze.erp_cust_az12;
+
+        PRINT '-----------------------------------------------';
+        PRINT '>>Inserting Data into: bronze.erp_cust_az12';
+        PRINT '-----------------------------------------------';
+        BULK INSERT bronze.erp_cust_az12
+        from 'D:\Projects\MS SQL & MSBI BY GURU PRASAD\Data Warehouse\git_repo\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+        WITH
+        (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '\n',
+            TABLOCK
+        );
+
+        --select * from bronze.erp_cust_az12;
+        select count(*) from bronze.erp_cust_az12;
+
+        -----data insertion into bronze.erp_px_cat_g1v2 tables
+
+        PRINT '-----------------------------------------------';
+        PRINT '>>Truncating Table: bronze.erp_px_cat_g1v2';
+        PRINT '-----------------------------------------------';
+        TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+        PRINT '-----------------------------------------------';
+        PRINT '>>Inserting Data into: bronze.erp_px_cat_g1v2';
+        PRINT '-----------------------------------------------';
+        BULK INSERT bronze.erp_px_cat_g1v2
+        from 'D:\Projects\MS SQL & MSBI BY GURU PRASAD\Data Warehouse\git_repo\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+        WITH
+        (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '\n',
+            TABLOCK
+        );
+
+        --select * from bronze.erp_px_cat_g1v2;
+        select count(*) from bronze.erp_px_cat_g1v2;
+    END TRY
+    BEGIN CATCH
+        PRINT 'Error Occurred While Loading the Bronze Layer';
+        PRINT 'Error Number: ' +  CAST(ERROR_NUMBER() AS NVARCHAR);
+        PRINT 'Error Message: ' + CAST(ERROR_STATE() AS NVARCHAR);
+    END CATCH;
+
+END
+GO
 

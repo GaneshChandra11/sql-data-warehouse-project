ccfdc3226be29d83ace7ec88289bd1998aeafe30
GaneshChandra11 <ganeshchandra.cs.it@gmail.com>
2025-08-30 13:21:22 +0530

    created folder inside script folder init_database.sql file created in scripts folder

---
+++ scripts/init_database.sql
@@ -0,0 +1,61 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists.
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas
+    within the database: 'bronze', 'silver', and 'gold'.
+
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists.
+    All data in the database will be permanently deleted. Proceed with caution
+    and ensure you have proper backups before running this script.
+*/
+
+
+
+
+use master;
+GO
+
+
+--drop and recreate Data Warehouse Database if it exists
+IF EXISTS (SELECT * FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END 
+
+
+
+--Create Data Warehouse Database
+CREATE DATABASE DataWarehouse;
+go
+
+
+use DataWarehouse;
+GO
+
+
+-----Create Bronze, Silver and Gold Schemas
+
+/*create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
+*/
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'bronze')
+    EXEC('CREATE SCHEMA bronze');
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'silver')
+    EXEC('CREATE SCHEMA silver');
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'gold')
+    EXEC('CREATE SCHEMA gold');
+GO
+

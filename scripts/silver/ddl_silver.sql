523ec93dc4349a5c29e8f3a28dd7cb42c44ab9ea
GaneshChandra11 <ganeshchandra.cs.it@gmail.com>
2025-08-31 17:00:54 +0530

    DDL script for the silver layer-done.

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,138 @@
+
+/*
+====================================================================
+DDL Script: Create silver Layer Tables
+====================================================================
+
+Script Purpose:
+   This script creates tables in the 'silver' schema, dropping existing tables
+   if they already exist.
+   Run this script to re-define the DDL structure of 'silver' Tables
+
+====================================================================
+*/
+use DataWarehouse;
+GO
+
+---------------------------------------------------silver layer Creation------------------------------------------------------------------
+
+
+---CRM Customer Info Table
+
+if OBJECT_ID('silver.crm_cust_info', 'u') IS NOT NULL
+drop table silver.crm_cust_info;
+GO
+
+CREATE TABLE silver.crm_cust_info(
+    cst_id INT,
+    cst_key NVARCHAR(50),
+    cst_first_name NVARCHAR(50),
+    cst_last_name NVARCHAR(50),
+    cst_material_status NVARCHAR(50),
+    cst_gndr NVARCHAR(50),
+    cast_created_date DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+    
+
+);
+GO
+
+
+select * from silver.crm_cust_info;
+
+---CRM Product Info Table
+
+if OBJECT_ID('silver.crm_prd_info', 'u') IS NOT NULL
+drop table silver.crm_prd_info;
+GO
+
+CREATE TABLE silver.crm_prd_info(
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost NVARCHAR(50),
+    prd_line NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt DATETIME,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+select * from silver.crm_prd_info;
+
+---CRM Sales Details Table
+
+if OBJECT_ID('silver.crm_sales_details', 'u') IS NOT NULL
+drop table silver.crm_sales_details;
+GO
+
+create table silver.crm_sales_details(
+    sls_ord_num NVARCHAR(50),
+    sls_prd_key NVARCHAR(50),
+    sls_cust_id INT,
+    sls_order_dt int,
+    sls_ship_dt int,
+    sls_due_dt int,
+    sls_sales int,
+    sls_quantity int,
+    sls_price int,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+select * from silver.crm_sales_details;
+
+
+--erp location_a101 table
+
+if OBJECT_ID('silver.erp_loc_a101', 'u') IS NOT NULL
+drop table silver.erp_loc_a101; 
+GO
+
+create table silver.erp_loc_a101(
+    cid NVARCHAR(50),
+    cntry nvarchar(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+    
+);
+GO
+
+select * from silver.erp_loc_a101;
+
+
+--erp customer_az12 table
+
+if OBJECT_ID('silver.erp_cust_az12', 'u') IS NOT NULL
+drop table silver.erp_cust_az12;
+GO
+
+create table silver.erp_cust_az12(
+    cid NVARCHAR(50),
+    bdate DATE,
+    gen NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+select * from silver.erp_cust_az12;
+
+--ERP px_cat_g1v2 table
+
+if OBJECT_ID('silver.erp_px_cat_g1v2', 'u') IS NOT NULL
+drop table silver.erp_px_cat_g1v2; 
+GO
+
+create table silver.erp_px_cat_g1v2(
+    id NVARCHAR(50),
+    cat NVARCHAR(50),
+    subcat NVARCHAR(50),
+    maintenance NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+select * from silver.erp_px_cat_g1v2;
+GO
+
+
+
